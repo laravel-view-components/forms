e3482c75bf11843e34f7145b2bc47fafa4ad1844
Daniel Wiser <dwiser@graboyes.com>
2020-05-13 09:39:29 -0400

    Update toggle with preset state

--- src/Components/Form/Toggle.php
+++ src/Components/Form/Toggle.php
@@ -19,19 +19,26 @@ class Toggle extends Component
      * @var string
      */
     public $name;
+    /**
+     * Whether or not the toggle is checked.
+     *
+     * @var bool
+     */
+    public $checked;
 
     /**
      * Create a new component instance.
      *
      * @param  string  $name
-     * @param  string  $checked
+     * @param  bool    $checked
      *
      * @return void
      */
-    public function __construct($name = "toggle")
+    public function __construct($name = "toggle", $checked = false)
     {
         $this->id = 'form-' . Str::kebab(class_basename(get_class($this))) . '-' . md5($name);
         $this->name = $name;
+        $this->checked = $checked;
     }
 
     /**
--- src/Views/form/toggle.blade.php
+++ src/Views/form/toggle.blade.php
@@ -1,5 +1,5 @@
 <label {{ $attributes->merge(['class' => 'form-check form-switch']) }}>
-  <input name="{{ $name }}" type="checkbox" class="form-check-input">
+  <input name="{{ $name }}" type="checkbox" class="form-check-input" {{ $checked ? 'checked' : old($name) }}>
   <span class="form-check-label">{{ $slot }}</span>
   @error($name)
   <div class="form-check-description invalid-feedback">{{ $message }}</div>
