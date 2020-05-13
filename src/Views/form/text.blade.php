67f983cfe1c3224063b27dae55cf6f988b437b5a
Dan Wiser <danswiser@users.noreply.github.com>
2020-05-13 11:45:09 -0400

    wip

--- src/Views/form/text.blade.php
+++ src/Views/form/text.blade.php
@@ -1,6 +1,6 @@
 <div {{ $attributes->merge(['class' => 'form-group']) }}>
   <label for="{{ $name }}" class="form-label">{{ $label }}</label>
-  <input name="{{ $name }}" type="text" class="form-control @error($name) is-invalid @enderror" placeholder="{{ $placeholder }}" autocomplete="off">
+  <input name="{{ $name }}" type="text" class="form-control @error($name) is-invalid @enderror" placeholder="{{ $placeholder }}" value="{{ old($name) }}">
   @error($name)
   <div class="invalid-feedback">{{ $message }}</div>
   @enderror
